b22d04158cbc5329b066147c5d1a2dc1e6f4c892
Anna_Gorova <koshka20.03.19@gmail.com>
2026-07-16 20:24:43 +0300

    Initial commit: Sales Analytics MCP server

---
+++ weather_demo.py
@@ -0,0 +1,12 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("WeatherServer")
+
+
+@mcp.tool
+def get_weather(city: str) -> str:
+    return f"Сонячно, +24 C у {city}"
+
+
+if __name__ == "__main__":
+    mcp.run()
